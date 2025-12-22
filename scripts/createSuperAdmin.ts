4bcdcb2f98d662dabdf24cdb5c516b70dbd22049
priya-Astrid <priya.yadav@astridinfotech.com>
2025-12-22 17:00:32 +0530

    secure: move superadmin credential to env for sucurity

--- scripts/createSuperAdmin.ts
+++ scripts/createSuperAdmin.ts
@@ -6,26 +6,34 @@ import { Role } from '../src/models/role.model';
 dotenv.config();
 const createSuperAdmin = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || '');
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI is missing ');
+    }
+    if (!process.env.SUPERADMIN_EMAIL || !process.env.SUPERADMIN_PASSWORD) {
+      throw new Error('SuperAdmin credentials are missing in env');
+    }
+
+    await mongoose.connect(process.env.MONGO_URI);
 
     let role = await Role.findOne({ role: 'superadmin' });
-    if(!role) {
+    if (!role) {
       role = await Role.create({ role: 'superadmin' });
       console.log('Role created: superadmin');
     }
-    const existing = await User.findOne({ email: 'superadmin@hospital.com' });
+    const existing = await User.findOne({
+      email: process.env.SUPERADMIN_EMAIL,
+    });
     if (!existing) {
-      const hashed = await bcrypt.hash('Super@123', 10);
+      const hashedPassword= await bcrypt.hash(process.env.SUPERADMIN_PASSWORD, 12);
       await User.create({
         firstname: 'Super',
         lastname: 'Admin',
-        email: 'superadmin@hospital.com',
-        password: hashed,
+        email: process.env.SUPERADMIN_EMAIL,
+        password: hashedPassword,
         role: role._id,
       });
-      console.log("Superadmin created successfully")
-    }
-    else{
+      console.log('Superadmin created successfully');
+    } else {
       console.log('superadmin already exists');
     }
 
